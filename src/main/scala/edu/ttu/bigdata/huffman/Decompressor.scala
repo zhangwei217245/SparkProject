b717cec4e03883c6364ddeaf43f29a2cd5039670
zhangwei217245 <zhangwei217245@gmail.com>
2014-11-29 21:51:22 -0600

    edu.ttu.bigdata.huffman

---
+++ src/main/scala/edu/ttu/bigdata/huffman/Decompressor.scala
@@ -0,0 +1,8 @@
+package edu.ttu.bigdata.huffman
+
+/**
+ * Created by zhangwei on 11/29/14.
+ */
+object Decompressor {
+
+}
--- src/main/scala/org/vv/spark/Compressor.scala
+++ src/main/scala/org/vv/spark/Compressor.scala
@@ -4,6 +4,8 @@ import org.apache.spark.{SparkContext, SparkConf}
 
 /**
  * Created by zhangwei on 11/29/14.
+ *
+ * NOTE: THIS IS ONLY FOR TEST, NOT FOR FINAL RELEASE.
  */
 object Compressor {
 	def main(args: Array[String]) {
